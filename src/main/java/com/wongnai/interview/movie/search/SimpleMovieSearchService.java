ccae7e270514c8e25a958148f1f3a4face9b83b3
Karn Klawmprasrerdh <karnklawmprasrerdh@gmail.com>
2019-06-28 20:57:11 +0700

    Filter by query compare movie.name to word

--- src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
+++ src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
@@ -1,6 +1,8 @@
 package com.wongnai.interview.movie.search;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -19,6 +21,15 @@ public class SimpleMovieSearchService implements MovieSearchService {
 		//TODO: Step 2 => Implement this method by using data from MovieDataService
 		// All test in SimpleMovieSearchServiceIntegrationTest must pass.
 		// Please do not change @Component annotation on this class
-		return null;
+		return movieDataService.fetchAll().stream()
+				.map(movieData -> {
+					Movie movie = new Movie(movieData.getTitle());
+					movie.setActors(movieData.getCast());
+					return movie;
+				}).filter(movie -> {
+					List<String> words = Arrays.asList(movie.getName().split(" "));
+					return words.stream().anyMatch(word ->
+							word.toLowerCase().equals(queryText.toLowerCase()));
+				}).collect(Collectors.toList());
 	}
 }
