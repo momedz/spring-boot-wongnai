7ff080604b5e2c1584ead494ef31d3a97e7b22e6
Karn Klawmprasrerdh <karnklawmprasrerdh@gmail.com>
2019-06-28 21:14:34 +0700

    Initial prepare information before start service

--- src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
+++ src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
@@ -2,11 +2,15 @@ package com.wongnai.interview.movie.sync;
 
 import javax.transaction.Transactional;
 
+import com.wongnai.interview.movie.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.wongnai.interview.movie.MovieRepository;
 import com.wongnai.interview.movie.external.MovieDataService;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class MovieDataSynchronizer {
@@ -19,5 +23,12 @@ public class MovieDataSynchronizer {
 	@Transactional
 	public void forceSync() {
 		//TODO: implement this to sync movie into repository
+		List<Movie> movies = movieDataService.fetchAll().stream()
+				.map(movieData -> {
+					Movie movie = new Movie(movieData.getTitle());
+					movie.setActors(movieData.getCast());
+					return movie;
+				}).collect(Collectors.toList());
+		movieRepository.saveAll(movies);
 	}
 }
